7f221b814e11b0d694d01c9ba5e2e495de2e91a9
DreadPirate09 <62398759+DreadPirate09@users.noreply.github.com>
2025-01-20 21:37:01 +0200

    Fix bounding box bug..

--- depth_prediction.py
+++ depth_prediction.py
@@ -7,6 +7,10 @@ import time
 import win32api
 import torch
 
+alpha = 0.2
+previous_depth = 0.0
+depth_scale = 1.0
+
 pygame.init()
 window_size = (800*2, 600)
 screen = pygame.display.set_mode(window_size)
@@ -64,18 +68,37 @@ while True:
         depth_prediction = depth_model(input_batch)
         depth_map = depth_prediction.squeeze().cpu().numpy()
     depth_map_normalized = (depth_map - np.min(depth_map)) / (np.max(depth_map) - np.min(depth_map))
+    print("Min and max+ "+str(np.min(depth_map))+" "+str(np.max(depth_map)))
     for box, conf, cls in zip(results[0].boxes.xyxy, results[0].boxes.conf, results[0].boxes.cls):
         x_min, y_min, x_max, y_max = map(int, box[:4])
         confidence = conf.item()
         class_id = int(cls.item())
 
         if class_id == 0 and confidence > 0.70:
-            object_depth = depth_map[y_min:y_max, x_min:x_max]
+            scale_x = depth_map.shape[1] / frame.shape[1]
+            scale_y = depth_map.shape[0] / frame.shape[0]
+
+            x_min_scaled = int(x_min * scale_x)
+            x_max_scaled = int(x_max * scale_x)
+            y_min_scaled = int(y_min * scale_y)
+            y_max_scaled = int(y_max * scale_y)
+
+            x_min_scaled = max(0, x_min_scaled)
+            x_max_scaled = min(depth_map.shape[1], x_max_scaled)
+            y_min_scaled = max(0, y_min_scaled)
+            y_max_scaled = min(depth_map.shape[0], y_max_scaled)
+
+            object_depth = depth_map[y_min_scaled:y_max_scaled, x_min_scaled:x_max_scaled]
+            print(object_depth)
 
             if object_depth.size > 0:
-                distance = np.mean(object_depth)
+                distance = np.max(depth_map) - np.mean(object_depth)
+                print(f'Object depth size {object_depth.size}')
             else:
-                distance = 0
+                print(f'Object depth size {object_depth.size}')
+                print(f'Bounding box: x_min={x_min}, x_max={x_max}, y_min={y_min}, y_max={y_max}')
+                print(f'Depth map shape: {depth_map.shape}')
+                distance = 99999
 
             cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
             cv2.putText(frame, f'Distance: {distance:.2f} units', (x_min, y_min - 10),
