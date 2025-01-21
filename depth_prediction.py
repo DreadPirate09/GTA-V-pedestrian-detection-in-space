051cf0fb0e15db760a1bf342e9d75ce61930e398
DreadPirate09 <62398759+DreadPirate09@users.noreply.github.com>
2025-01-21 23:19:03 +0200

    Used the yolo11n-seg.pt for getting the segmentation as well so we can calculate the mean of the segmentated area, getting a better accuracy in depth

--- depth_prediction.py
+++ depth_prediction.py
@@ -6,6 +6,8 @@ import pygame
 import time
 import win32api
 import torch
+import matplotlib.pyplot as plt
+
 
 alpha = 0.2
 previous_depth = 0.0
@@ -17,7 +19,7 @@ screen = pygame.display.set_mode(window_size)
 pygame.display.set_caption("GTAV constructed image")
 clock = pygame.time.Clock()
 
-model = YOLO('yolov8n.pt')
+model = YOLO('yolo11n-seg.pt')
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 model.to(DEVICE)
 
@@ -69,12 +71,18 @@ while True:
         depth_map = depth_prediction.squeeze().cpu().numpy()
     depth_map_normalized = (depth_map - np.min(depth_map)) / (np.max(depth_map) - np.min(depth_map))
     print("Min and max+ "+str(np.min(depth_map))+" "+str(np.max(depth_map)))
-    for box, conf, cls in zip(results[0].boxes.xyxy, results[0].boxes.conf, results[0].boxes.cls):
+    for mask, box, conf, cls in zip(results[0].masks.data, results[0].boxes.xyxy, results[0].boxes.conf, results[0].boxes.cls):
         x_min, y_min, x_max, y_max = map(int, box[:4])
         confidence = conf.item()
         class_id = int(cls.item())
 
         if class_id == 0 and confidence > 0.70:
+
+            segmentation_mask = mask.cpu().numpy() > 0.5
+            segmentation_mask_resized = cv2.resize(segmentation_mask.astype(np.uint8), (depth_map.shape[1], depth_map.shape[0]))
+            segmentation_mask_resized = segmentation_mask_resized > 0
+            object_depth = depth_map[segmentation_mask_resized]
+
             scale_x = depth_map.shape[1] / frame.shape[1]
             scale_y = depth_map.shape[0] / frame.shape[0]
 
@@ -91,9 +99,6 @@ while True:
             pos_x = (x_min_scaled + x_max_scaled) / 2
             pos_y = (y_min_scaled + y_max_scaled) / 2 
 
-            object_depth = depth_map[y_min_scaled:y_max_scaled, x_min_scaled:x_max_scaled]
-            print(object_depth)
-
             if object_depth.size > 0:
                 distance = np.max(depth_map) - np.mean(object_depth)
                 print(f'Object depth size {object_depth.size}')
@@ -103,9 +108,12 @@ while True:
                 print(f'Depth map shape: {depth_map.shape}')
                 distance = 99999
 
+            text_to_add = f'X: {pos_x:.2f} \nY: {pos_y:.2f} \nZ: {distance:.2f} units'
+
             cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
-            cv2.putText(frame, f'X: {pos_x:.2f} Y: {pos_y:.2f} Distance: {distance:.2f} units', (x_min, y_min - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            for pos,t in enumerate(text_to_add.split('\n')):
+                cv2.putText(frame, t, (x_min, y_min + 15 * pos + 15),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 0), 1)
 
 
     depth_map_vis = (depth_map_normalized * 255).astype(np.uint8)
